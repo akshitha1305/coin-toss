ee84dea130e2e8abcdc3a42c83c352d72faca5b7
akshitha1305 <akshithamerugu321@gmail.com>
2023-01-02 05:22:56 +0000

    display

---
+++ src/components/CoinToss/index.js
@@ -0,0 +1,67 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+const headImage = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
+const tailImage = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
+
+class CoinToss extends Component {
+  state = {
+    isHeads: 'True',
+    img: {headImage},
+    totalCount: 0,
+    headsCount: 0,
+    tailsCount: 0,
+  }
+
+  onClickTossButt = () => {
+    const tossResult = Math.floor(Math.random() * 2)
+
+    if (tossResult === 0) {
+      this.setState({isHeads: 'True'})
+    } else {
+      this.setState({isHeads: 'False'})
+    }
+    const {isHeads} = this.state
+    if (isHeads === 'True') {
+      this.setState(prevState => ({headsCount: prevState.headsCount + 1}))
+      this.setState(prevState => ({totalCount: prevState.totalCount + 1}))
+      this.setState({
+        img: {headImage},
+      })
+    } else if (isHeads === 'False') {
+      this.setState(prevState => ({tailsCount: prevState.tailsCount + 1}))
+      this.setState(prevState => ({totalCount: prevState.totalCount + 1}))
+      this.setState({
+        img: {tailImage},
+      })
+    }
+  }
+
+  render() {
+    const {img, totalCount, headsCount, tailsCount} = this.state
+    return (
+      <div className="bg-container">
+        <div className="coin-card">
+          <h1 className="heading">Coin Toss Game</h1>
+          <p className="para">Heads (or) Tails</p>
+          <img src={img} className="coin-img" alt="toss result" />
+          <button
+            onClick={this.onClickTossButt}
+            type="button"
+            className="toss-button"
+          >
+            Toss Coin
+          </button>
+          <div className="count-result-container">
+            <p className="count-para">Total:{totalCount}</p>
+            <p className="count-para">Heads:{headsCount}</p>
+            <p className="count-para">Tails:{tailsCount}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CoinToss
